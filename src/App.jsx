5618e282264c57944d7fa3ea83421c860e4325fd
Nergiz Şengün <nergizduygu56@gmail.com>
2025-03-16 15:10:57 +0300

    design created

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import TodoCreate from './components/TodoCreate'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='App' >
+      <div className='todoCreate' >
+       <TodoCreate/>
+      </div>
+     
+      
+        
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/TodoCreate.jsx
@@ -0,0 +1,13 @@
+import '../App.css';
+
+export default function TodoCreate(){
+
+
+    return(
+        <div className="todo-create">
+            <input type="text" placeholder="enter task" />
+            <button>Todo Create</button>
+        
+        </div>
+    )
+}
